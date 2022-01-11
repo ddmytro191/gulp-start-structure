2d0314f02587775ad8c26d457dc176a119df8370
Dmytro Domaretskyi <dmytro.domaretskyi@gmail.com>
2022-01-11 20:26:47 +0200

    added error notifications

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -1,7 +1,11 @@
 import replace from 'gulp-replace'
+import plumber from 'gulp-plumber'
+import notify from 'gulp-notify'
 
 const plugins = {
-    replace
+    replace,
+    plumber,
+    notify
 }
 
 export default plugins
