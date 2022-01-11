0501abd03e4f4660072b474b0a1841bd7798c76e
Dmytro Domaretskyi <dmytro.domaretskyi@gmail.com>
2022-01-11 20:20:38 +0200

    added versions to avoid cache issues

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,11 +1,23 @@
 import fileinclude from 'gulp-file-include'
 import webpHtmlNoSvg from 'gulp-webp-html-nosvg'
+import versionNumber from 'gulp-version-number'
 
 const html = () => {
     return app.gulp.src(app.path.src.html)
         .pipe(fileinclude())
         .pipe(app.plugins.replace(/@img\//g, 'img/'))
         .pipe(webpHtmlNoSvg())
+        .pipe(versionNumber({
+            'value': '%DT%',
+            'append': {
+                'key': 'v',
+                'cover': 0,
+                'to': [ 'css', 'js' ],
+            },
+            'output': {
+                'file': 'gulp/version.json'
+            }
+        }))
         .pipe(app.gulp.dest(app.path.build.html))
 }
 
