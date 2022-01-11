7ae9225e99fb0d6296f0a7ed0d994bd7250ec34e
Dmytro Domaretskyi <dmytro.domaretskyi@gmail.com>
2022-01-11 19:47:48 +0200

    added html task

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,16 @@ import gulp from 'gulp'
 import path from './gulp/config/path.js'
 import reset from './gulp/tasks/reset.js'
 import copy from './gulp/tasks/copy.js'
+import html from './gulp/tasks/html.js'
 
 global.app = { gulp, path }
 
-const dev = gulp.series(reset, copy, watcher)
+const mainTasks = gulp.parallel(copy, html)
+const dev = gulp.series(reset, mainTasks, watcher)
 
 gulp.task('default', dev)
 
 function watcher() {
     gulp.watch(path.watch.files, gulp.series(reset, copy))
+    gulp.watch(path.watch.html, html)
 }
